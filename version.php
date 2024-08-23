06d62750d38f48f7b3a297f8b4f10e8ed36ff305
Snickser <snickser@gmail.com>
2024-08-23 10:44:32 +0300

    version

--- version.php
+++ version.php
@@ -27,3 +27,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2024082301;      // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023100400;      // Requires this Moodle version.
 $plugin->component = 'media_rutube'; // Full name of the plugin (used for diagnostics).
+$plugin->release   = '0.1';
