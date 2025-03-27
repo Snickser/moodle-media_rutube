c790b433aa5f616b37070b245376de97830863fa
Snickser <snickser@gmail.com>
2025-03-27 22:08:22 +0300

    tune

--- settings.php
+++ settings.php
@@ -24,6 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+global $CFG;
+
 if ($ADMIN->fulltree) {
     $plugininfo = \core_plugin_manager::instance()->get_plugin_info('media_rutube');
     $donate = get_string('donate', 'media_rutube', $plugininfo);
